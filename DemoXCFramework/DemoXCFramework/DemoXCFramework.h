7bfd0cf906e72124fbae67214bb78ffd6583db0d
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-22 11:26:02 +0900

    naming 변경

---
+++ DemoXCFramework/DemoXCFramework/DemoXCFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoXCFramework.h
+//  DemoXCFramework
+//
+//  Created by Inpyo Hong on 2021/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoXCFramework.
+FOUNDATION_EXPORT double DemoXCFrameworkVersionNumber;
+
+//! Project version string for DemoXCFramework.
+FOUNDATION_EXPORT const unsigned char DemoXCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoframework/PublicHeader.h>
+
+
